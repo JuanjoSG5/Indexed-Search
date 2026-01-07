38864e011222d5cfc137aedb5320f0a8114d9919
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2026-01-07 10:16:39 +0100

    feat: updating package for netlify deployment

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,17 @@
+// app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: ':lang',
+    renderMode: RenderMode.Server, // <--- Change to Server (no params needed)
+  },
+  {
+    path: ':lang/architecture',
+    renderMode: RenderMode.Server, // <--- Change to Server
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
